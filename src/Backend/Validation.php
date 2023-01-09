3d3b316e64974da6f35975e115d3be04e13b949d
AnujDagade <dagadeanuj@gmail.com>
2023-01-09 15:56:30 +0000

    csv implemented

---
+++ src/Backend/Validation.php
@@ -0,0 +1,17 @@
+<?php
+
+    $name = $_POST['uname'];
+    $email = $_POST['uemail'];
+    $phone = $_POST['uphone'];
+
+    echo "$name<br>$email<br>$phone";
+
+    $fh = fopen("DB/uinfo.csv","r+");
+    if(!$fh) die ("File not found");
+
+    fseek($fh,0,SEEK_END);
+    echo ftell($fh);
+    fwrite($fh, $name.",".$email.",".$phone."\n");
+    
+
+?>
